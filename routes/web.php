5b26cfb2031646faa5bdb38114ecdd39b8016e1f
orca2818 <67529460+orca2818@users.noreply.github.com>
2021-06-28 14:14:02 +0900

    Branch Create-login(2021/06/28 14:14)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,14 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return view('/user/login');
+    return view('welcome');
 });
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user/edit/{id}', [UserController::class, 'edit']);
-Route::delete('/user/delete/{id}', [UserController::class, 'delete']);
-Route::put('/user/update/{id}', [UserController::class, 'update']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::post('/home', [UserController::class, 'home']);
+require __DIR__.'/auth.php';
