79b26e8bbd8165cc8632ad1471b1444c12f9252e
orca2818 <67529460+orca2818@users.noreply.github.com>
2021-07-01 16:32:15 +0900

    Branch Create-login(2021/07/01 16:32)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,12 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        
+        @isset($head)
+        {{ $head }}
+        @endisset
+        
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
